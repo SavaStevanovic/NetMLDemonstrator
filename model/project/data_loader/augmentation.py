Loss expanded to acount for box sizes and batch normalization problem fixed.

model/project/data_loader/augmentation.py
@@ -78,11 +78,11 @@ class TargetTransformToBoxes(object):
 
     def __call__(self, target):
         first_target = target[0,...]
-        objects = np.argwhere(first_target[::(5+self.classes_len)]>0.5).T
+        objects = np.argwhere(first_target[::(len(self.ratios)*5+self.classes_len)]>0.5).T
         labels = []
         for cords in objects:
             l = {}
-            class_id = np.argmax(first_target[cords[0]+5:cords[0]+5+self.classes_len, cords[1], cords[2]])
+            class_id = np.argmax(first_target[cords[0]+5:cords[0]+len(self.ratios)*5+self.classes_len, cords[1], cords[2]])
             l['category_id'] = class_id
             box_center = first_target[cords[0]+3, cords[1], cords[2]], first_target[cords[0]+4, cords[1], cords[2]]
             box_size = first_target[cords[0]+1, cords[1], cords[2]], first_target[cords[0]+2, cords[1], cords[2]]

model/project/model/resnet.py
@@ -18,7 +18,7 @@ def convUp(in_planes, out_planes):
 class PreActivationBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, norm_layer=nn.BatchNorm2d):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, norm_layer=nn.InstanceNorm2d):
         super(PreActivationBlock, self).__init__()
         self.bn1 = norm_layer(inplanes)
         self.relu = nn.ReLU(inplace=True)
@@ -51,7 +51,7 @@ class PreActivationBlock(nn.Module):
 
 class ResNetBackbone(nn.Module):
 
-    def __init__(self, block, layers, norm_layer=nn.BatchNorm2d, multiplier=2):
+    def __init__(self, block, layers, norm_layer=nn.InstanceNorm2d, multiplier=2):
         super(ResNetBackbone, self).__init__()
 
         self.groups = 1

model/project/model_fitting/losses.py
@@ -1,6 +1,8 @@
 import torch.nn.functional as F
 import torch
 from sklearn.metrics import f1_score
+import numpy as np
+
 class ContrastiveLoss(torch.nn.Module):
     """
     Contrastive loss function.
@@ -23,37 +25,40 @@ class YoloLoss(torch.nn.Module):
         super(YoloLoss, self).__init__()
         self.classes_len = classes_len
         self.ratios = ratios
+        self.l1_loss = torch.nn.L1Loss(reduction='none')
 
     def forward(self, outputs, labels):
         loss = 0
+        total_objectness_loss = 0
+        total_size_loss = 0
+        objectnes_f1_scores = []
         for batch in range(labels.size()[0]):
             output, label = outputs[batch], labels[batch]
-            obj_objectness = torch.sigmoid(output[::(5*len(self.ratios)+self.classes_len)])
-            lab_objectness = label[::(5*len(self.ratios)+self.classes_len)]
+            object_range = 5*len(self.ratios)+self.classes_len
+            obj_objectness = torch.sigmoid(output[::object_range])
+            lab_objectness = label[::object_range]
             obj_objectness = torch.flatten(obj_objectness)
             lab_objectness = torch.flatten(lab_objectness)
-            # loss += torch.sum(lab_objectness * (lab_objectness - obj_objectness) ** 2)
-            # loss += torch.sum(0.5 * (1 - lab_objectness) * (lab_objectness - obj_objectness) ** 2)
-            # loss += (1-obj_objectness).dot(F.binary_cross_entropy(obj_objectness, lab_objectness.type(torch.cuda.FloatTensor), reduction='none'))/len(obj_objectness)
-            # loss += F.binary_cross_entropy(obj_objectness, lab_objectness.type(torch.cuda.FloatTensor))
-            loss+= self.focal_loss(obj_objectness, lab_objectness)
-        return loss, f1_score(lab_objectness.cpu(), obj_objectness.cpu()>0.5)
+            total_objectness_loss+= self.focal_loss(obj_objectness, lab_objectness)
+
+            size_range = [1,2]
+            box_size_range = [i for i in range(label.shape[0]) if i%object_range in size_range]
+            obj_box_size = torch.flatten(output[box_size_range])
+            lab_box_size = torch.flatten(label[box_size_range])
+            lab_size_objectness = torch.cat([lab_objectness for _ in size_range], 0)
+            total_size_loss += torch.sum(lab_size_objectness * self.l1_loss(obj_box_size, lab_box_size))
+            objectnes_f1_scores.append(f1_score(lab_objectness.cpu(), obj_objectness.cpu()>0.5))
+        objectnes_f1_score = np.average(objectnes_f1_scores)
+        loss = total_objectness_loss + total_size_loss
+        return loss, objectnes_f1_score, total_objectness_loss, total_size_loss
 
     def focal_loss(self, x, y):
-        '''Focal loss.
-        Args:
-          x: (tensor) sized [N,D].
-          y: (tensor) sized [N,].
-        Return:
-          (tensor) focal loss.
-        '''
         alpha = 1
         gamma = 1
         y = y.unsqueeze(1)
         x = x.unsqueeze(1)
 
         y = torch.cat([y, 1-y], 1)
-        # t = t.cuda() 
         x = torch.cat([x, 1-x], 1).clamp(1e-8, 1. - 1e-8)
 
         F_loss = -y * (1 - x) * torch.log(x)

model/project/model_fitting/metrics.py
@@ -15,24 +15,25 @@ def metrics( net, dataloader, box_transform, epoch=1):
     with torch.no_grad():
         for i, data in enumerate(dataloader):
             image, labels = data
-            outputs = torch.sigmoid(net(image.cuda())).cpu()
-            boxes_pr = box_transform(outputs)
-            # if len(boxes_pr)>0:
-            #     print('Alaljulja')
+            outputs = net(image.cuda()).cpu()
             boxes_tr = box_transform(labels)
+            boxes_pr = box_transform(outputs.cpu().detach())
             metched =[False for x in boxes_tr]
             true_positives = 0
-
             for b in boxes_pr:
                 for i, true_b in enumerate(boxes_tr):  
-                    if b['category_id'] != true_b['category_id']:
-                        continue
-                    if IoU(b['bbox'], true_b['bbox'])>0.5:
+                    # if b['category_id'] != true_b['category_id']:
+                    #     continue
+                    if IoU(b['bbox'], true_b['bbox'])>0.1:
                         metched[i]=True
                         true_positives+=1
             average_precision.append(true_positives/(len(boxes_pr)+0.1**9))
-
+            
             if i>len(dataloader)-5:
+                object_range = 5*len(net.ratios)+net.classes
+                outputs[:, ::object_range] = torch.sigmoid(outputs[:, ::object_range])
+                boxes_pr = box_transform(outputs.cpu().detach())
+                boxes_tr = box_transform(labels.cpu().detach())
                 pilImage = torchvision.transforms.ToPILImage()(image[0,...])
                 draw = ImageDraw.Draw(pilImage)
                 for l in boxes_pr:
@@ -44,6 +45,7 @@ def metrics( net, dataloader, box_transform, epoch=1):
                     draw.rectangle(((bbox[0], bbox[1]), (bbox[0]+bbox[2], bbox[1]+bbox[3])), outline = 'blue')
                     draw.text((bbox[0], bbox[1]-10), dataloader.cats[l['category_id']][1], outline = 'blue')
                 images.append(np.array(pilImage))
+                
     return sum(average_precision)/len(average_precision), images
 
 

model/project/model_fitting/train.py
@@ -12,28 +12,51 @@ from pycocotools.cocoeval import COCOeval
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance
 import torchvision
 import random
+import numpy as np
 
-def fit_epoch(net, trainloader, writer, lr_rate, box_transform, epoch=1):
+def fit_epoch(net, dataloader, writer, lr_rate, box_transform, epoch=1):
     net.train()
     optimizer = optim.Adam(net.parameters(), lr_rate)
     criterion = YoloLoss(classes_len = net.classes, ratios = net.ratios)
-    loss = 0.0
+    losses = 0.0
     objectness_f1s = 0.0
-    for i, data in enumerate(tqdm(trainloader)):
+    total_objectness_loss = 0.0
+    total_size_loss = 0.0
+    images = []
+    for i, data in enumerate(tqdm(dataloader)):
         # get the inputs; data is a list of [inputs, labels]
         image, labels = data
         # zero the parameter gradients
         optimizer.zero_grad()
         # forward + backward + optimize
         outputs = net(image.cuda())
-        loss, objectness_f1 = criterion(outputs, labels.cuda())
+        loss, objectness_f1, objectness_loss, size_loss = criterion(outputs, labels.cuda())
         loss.backward()
         optimizer.step()
-
-        loss += loss.item()
+        total_objectness_loss += objectness_loss.item()
+        total_size_loss += size_loss.item()
+        losses += loss.item()
         objectness_f1s +=objectness_f1
 
-    return loss/len(trainloader), objectness_f1s/len(trainloader)
+        if i>len(dataloader)-5:
+            object_range = 5*len(net.ratios)+net.classes
+            outputs[:, ::object_range] = torch.sigmoid(outputs[:, ::object_range])
+            boxes_pr = box_transform(outputs.cpu().detach())
+            boxes_tr = box_transform(labels.cpu().detach())
+            pilImage = torchvision.transforms.ToPILImage()(image[0,...])
+            draw = ImageDraw.Draw(pilImage)
+            for l in boxes_pr:
+                bbox = l['bbox']
+                draw.rectangle(((bbox[0], bbox[1]), (bbox[0]+bbox[2], bbox[1]+bbox[3])), outline = 'red')
+                draw.text((bbox[0], bbox[1]-10), dataloader.cats[l['category_id']][1], outline = 'red')
+            for l in boxes_tr:
+                bbox = l['bbox']
+                draw.rectangle(((bbox[0], bbox[1]), (bbox[0]+bbox[2], bbox[1]+bbox[3])), outline = 'blue')
+                draw.text((bbox[0], bbox[1]-10), dataloader.cats[l['category_id']][1], outline = 'blue')
+            images.append(np.array(pilImage))
+
+    data_len = len(dataloader)
+    return losses/data_len, objectness_f1s/data_len, total_objectness_loss/data_len, total_size_loss/data_len, images
 
 def fit(net, trainloader, validationloader, chp_prefix, box_transform, epochs=1000, lower_learning_period=10):
     log_datatime = str(datetime.now().time())
@@ -42,10 +65,10 @@ def fit(net, trainloader, validationloader, chp_prefix, box_transform, epochs=10
     i = 0
     lr_rate = 0.0001
     for epoch in range(epochs):
-        loss, objectness_f1 = fit_epoch(net, trainloader, writer, lr_rate, box_transform, epoch=epoch)
-        train_map, train_samples = metrics(net, trainloader, box_transform, epoch)
+        loss, objectness_f1, objectness_loss, size_loss, train_samples = fit_epoch(net, trainloader, writer, lr_rate, box_transform, epoch=epoch)
+        # train_map, train_samples = metrics(net, trainloader, box_transform, epoch)
         validation_map, validation_samples = metrics(net, validationloader, box_transform, epoch)
-        writer.add_scalars('metrics', {'train_map':train_map, 'validation_map':validation_map, 'train_loss':loss, 'objectness_f1':objectness_f1}, epoch)
+        writer.add_scalars('metrics', {'validation_map':validation_map, 'train_loss':loss, 'objectness_f1':objectness_f1, 'objectness_loss': objectness_loss, 'size_loss':size_loss}, epoch)
         for sample in train_samples:
             writer.add_images('train_sample', sample, epoch, dataformats='HWC')
         for sample in validation_samples: