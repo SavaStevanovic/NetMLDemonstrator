7f3272fc438011265705e50e4e7983b12ce9d4dd
Sava Stevanovic <sava.stevanovic@pmf.edu.rs>
2020-04-19 13:08:46 +0200

    Small fixes refactoring and gitignore update

--- client/project/service.py
+++ client/project/service.py
@@ -13,12 +13,6 @@ video_stream = VideoCamera()
 def index():
     return render_template('index.html')
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
 @app.route('/frame_upload', methods=['GET', 'POST'])
 def frame_upload():
     nparr = np.fromstring(request.data, np.uint8)
@@ -27,11 +21,8 @@ def frame_upload():
     
     retval, buffer = cv2.imencode('.jpeg', img)
     data = {'image':base64.b64encode(buffer).decode("utf-8") }
-    # encode response using jsonpickle
-    # response_pickled = json.dumps(data, ensure_ascii=False)
 
-    # return Response(response=data, status=200, mimetype="application/json")
     return data
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=False, port="5001")
+    app.run(host='0.0.0.0', debug=False, port="5001", threaded=True)
--- model/project/main.py
+++ model/project/main.py
@@ -3,12 +3,18 @@ from model.networks import YoloNet, ResNetBackbone, PreActivationBlock
 from model_fitting.train import fit
 from data_loader.coco_dataset import CocoDetectionDatasetProvider
 
-th_count = 1
+th_count = 12
 ratios = [1.0]
+preload = True
+
 coco_provider = CocoDetectionDatasetProvider(annDir='/Data/Coco/', th_count=th_count, ratios=ratios)
 
 backbone = ResNetBackbone(block = PreActivationBlock, layers = [2, 2, 2, 2])
-net = YoloNet(backbone, classes = coco_provider.classes, ratios=ratios)
+
+if preload:
+    net = torch.load('checkpoints/coco_checkpoints.pth')
+else:
+    net = YoloNet(backbone, classes = coco_provider.classes, ratios=ratios)
 net.cuda()
 
 fit(net, coco_provider.trainloader, coco_provider.validationloader, chp_prefix="coco", box_transform = coco_provider.target_to_box_transform, epochs=1000, lower_learning_period=20)
