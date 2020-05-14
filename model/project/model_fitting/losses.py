f2fc35c5f2012642dd609feba00501b66b1aaddb
Sava Stevanovic <sava.stevanovic@pmf.edu.rs>
2020-05-14 13:09:22 +0200

    Optimization of loss calculation

--- model/project/model_fitting/losses.py
+++ model/project/model_fitting/losses.py
@@ -23,35 +23,33 @@ class YoloLoss(torch.nn.Module):
         loss = 0.0
         batch_size = labels.shape[0]
         for batch in range(batch_size):
-            output_cells, labels_cells = outputs[batch], labels[batch]
-            for i in range(labels.shape[1]):
-                output, label = output_cells[i], labels_cells[i]
-                obj_objectness = torch.flatten(output[self.ranges.objectness])
-                lab_objectness = torch.flatten(label[self.ranges.objectness])
-                objectness_loss = self.focal_loss(obj_objectness, lab_objectness)
-                loss += objectness_loss
-                total_objectness_loss += objectness_loss.item()
+            output, label = outputs[batch], labels[batch]
+            obj_objectness = output[:, self.ranges.objectness].flatten(1)
+            lab_objectness = label[:, self.ranges.objectness].flatten(1)
+            objectness_loss = self.focal_loss(obj_objectness, lab_objectness)
+            loss += objectness_loss
+            total_objectness_loss += objectness_loss.item()
 
-                obj_box_size = torch.flatten(output[self.ranges.size])
-                lab_box_size = torch.flatten(label[self.ranges.size])
-                lab_size_objectness = torch.cat([lab_objectness for _ in self.ranges.size], 0)
-                size_loss = self.size_scale * lab_size_objectness.dot(self.l1_loss(obj_box_size, lab_box_size))
-                loss += size_loss
-                total_size_loss += size_loss.item()
+            obj_box_size = output[:, self.ranges.size].flatten(2)
+            lab_box_size = label[:, self.ranges.size].flatten(2)
+            size_loss = self.size_scale * lab_objectness.unsqueeze(1)*self.l1_loss(obj_box_size, lab_box_size)
+            size_loss = size_loss.sum()
+            loss += size_loss
+            total_size_loss += size_loss.item()
 
-                obj_offset = torch.flatten(output[self.ranges.offset])
-                lab_offset = torch.flatten(label[self.ranges.offset])
-                lab_offset_objectness = torch.cat([lab_objectness for _ in self.ranges.offset], 0)
-                offset_loss = self.offset_scale * lab_offset_objectness.dot(self.l1_loss(obj_offset, lab_offset))
-                loss += offset_loss
-                total_offset_loss += offset_loss.item()
+            obj_offset = output[:, self.ranges.offset].flatten(2)
+            lab_offset = label[:, self.ranges.offset].flatten(2)
+            offset_loss = self.offset_scale * lab_objectness.unsqueeze(1)*self.l1_loss(obj_offset, lab_offset)
+            offset_loss = offset_loss.sum()
+            loss += offset_loss
+            total_offset_loss += offset_loss.item()
 
-                obj_class = output[self.ranges.classes].unsqueeze(0)
-                lab_class = label[self.ranges.classes].unsqueeze(0).argmax(1)
-                lab_class_objectness = lab_objectness
-                class_loss = self.class_scale * lab_class_objectness.dot(torch.flatten(self.class_loss(obj_class, lab_class)))
-                loss += class_loss
-                total_class_loss += class_loss.item()
+            obj_class = output[:, self.ranges.classes].flatten(2)
+            lab_class = label[:, self.ranges.classes].flatten(2).argmax(1)
+            lab_class_objectness = lab_objectness
+            class_loss = self.class_scale * lab_class_objectness.flatten().dot(self.class_loss(obj_class, lab_class).flatten())
+            loss += class_loss
+            total_class_loss += class_loss.item()
 
         # loss = total_objectness_loss + total_size_loss + total_offset_loss + total_class_loss
         return loss/batch_size, total_objectness_loss/batch_size, total_size_loss/batch_size, total_offset_loss/batch_size, total_class_loss/batch_size
@@ -59,11 +57,11 @@ class YoloLoss(torch.nn.Module):
     def focal_loss(self, x, y):
         alpha = 1
         gamma = 1
-        y = y.unsqueeze(1)
-        x = x.unsqueeze(1)
+        y = y.unsqueeze(-1)
+        x = x.unsqueeze(-1)
 
-        y = torch.cat([y, 1-y], 1)
-        x = torch.cat([x, 1-x], 1).clamp(1e-8, 1. - 1e-8)
+        y = torch.cat([y, 1-y], -1)
+        x = torch.cat([x, 1-x], -1).clamp(1e-8, 1. - 1e-8)
 
         F_loss = -y * (1 - x) * torch.log(x)
 
