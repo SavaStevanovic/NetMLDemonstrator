abfbc8fa3d9d63fb9b3445ec6dcea3c3fae84652
Sava Stevanovic <sava.stevanovic@pmf.edu.rs>
2020-07-11 15:25:49 +0200

    Streamlined container startup

--- application/docker.sh
+++ application/docker.sh
@@ -1,3 +1,3 @@
 docker build -t node14playground .
 xhost + 
-docker run --rm --name menager -e DISPLAY=$DISPLAY -p 4321:4321 -p 4200:4200 -it -v `pwd`/project:/app -v /tmp/.X11-unix:/tmp/.X11-unix  node14playground
+docker run --rm --name menager -e DISPLAY=$DISPLAY -p 4321:4321 -p 4200:4200 -dit -v `pwd`/project:/app -v /tmp/.X11-unix:/tmp/.X11-unix  node14playground
---
+++ run_dev_project.sh
@@ -0,0 +1,8 @@
+./docker_clean.sh
+cd application 
+./docker.sh
+cd ..
+cd model
+./docker.sh
+cd ..
+./docker_network.sh
