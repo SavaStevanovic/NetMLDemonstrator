81de146eaf0652e9595177f51124502544c494f3
Sava Stevanovic <sava.stevanovic@pmf.edu.rs>
2020-10-10 11:04:40 +0200

    Refactoring and style update

--- model/project/service.py
+++ model/project/service.py
@@ -19,7 +19,6 @@ model_paths = {
 
 class BaseHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
-        # print("setting headers!!!")
         self.set_header("Access-Control-Allow-Origin", "*")
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
         self.set_header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST')
