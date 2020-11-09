8bbfe2013f2df6f32b3f3d40bf3146a0c07e3dcb
sava <sava.stevanovic@pmf.edu.rs>
2020-11-09 20:28:08 +0100

    Changes and fixes for updated ubuntu
    Fix for docker not seen due to cached container.
    Updated path due to ubuntu 20.4 disk path change

--- model/docker.sh
+++ model/docker.sh
@@ -1,3 +1,3 @@
 docker build -t pytorch2001playground .
 xhost + 
-docker run --rm --name detection -e DISPLAY=$DISPLAY --ipc=host --gpus all -p 6006:6006 -p 5001:5001 -dit -v `pwd`/project:/app -v /media/Data/Coco:/Data/Coco -v /tmp/.X11-unix:/tmp/.X11-unix  pytorch2001playground
+docker run --rm --name detection -e DISPLAY=$DISPLAY --ipc=host --gpus all -p 6006:6006 -p 5001:5001 -dit -v `pwd`/project:/app -v /media/sava/Data/Coco:/Data/Coco -v /tmp/.X11-unix:/tmp/.X11-unix  pytorch2001playground
