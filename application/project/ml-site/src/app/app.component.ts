486b88af70713c3a95093e5dc2a08d3210bea200
sava <sava.stevanovic@pmf.edu.rs>
2020-11-18 20:45:41 +0100

    Fixed site scaling
    Site scaling was broken by adressbar

--- application/project/ml-site/src/app/app.component.ts
+++ application/project/ml-site/src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  @HostListener('window:resize', [])
+  onResize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
 }
