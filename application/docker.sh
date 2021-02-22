a8cee2dd146727fe9cffe23624ee85ae92467f1f
sava <sava.stevanovic@pmf.edu.rs>
2021-02-22 17:17:21 +0100

    Angular updated and missing hammer support added

--- application/docker.sh
+++ application/docker.sh
@@ -1,3 +1,3 @@
-docker build -t node14playground .
+docker build -t nodeplayground .
 xhost + 
-docker run --rm --name menager -e DISPLAY=$DISPLAY -p 4321:4321 -p 4200:4200 -p 4320:4320 -dit -v `pwd`/project:/app -v /tmp/.X11-unix:/tmp/.X11-unix  node14playground
+docker run --rm --name menager -e DISPLAY=$DISPLAY -p 4321:4321 -p 4200:4200 -p 4320:4320 -dit -v `pwd`/project:/app -v /tmp/.X11-unix:/tmp/.X11-unix  nodeplayground
