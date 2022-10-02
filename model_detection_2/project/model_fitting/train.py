b32565c2a24b367df13635d320eeb5b284ab3f43
sava <sava.stevanovic@pmf.edu.rs>
2022-10-03 00:00:06 +0200

    Enabled transfer learning

--- model_detection_2/project/model_fitting/train.py
+++ model_detection_2/project/model_fitting/train.py
@@ -88,10 +88,12 @@ def fit(net, trainloader, validationloader, dataset_name, epochs=1000, lower_lea
         else:
             train_config.iteration_age+=1
             print('Epoch {} metric: {}'.format(epoch, loss))
-        if train_config.iteration_age==lower_learning_period:
+        if train_config.iteration_age and (train_config.iteration_age % lower_learning_period) == 0:
             train_config.learning_rate*=0.5
-            train_config.iteration_age=0
             print("Learning rate lowered to {}".format(train_config.learning_rate))
+        if train_config.iteration_age == 1 + lower_learning_period:
+            net.grad_backbone(True)
+            print("Model unfrozen")
         train_config.epoch = epoch+1
         train_config.save(checkpoint_conf_path)
         torch.save(net, checkpoint_name_path)
