b9109842b087a31fe2c5a0675e04cfeec9d6b892
sava <sava.stevanovic@pmf.edu.rs>
2022-10-03 22:47:11 +0200

    Map logging

--- model_detection_2/project/model_fitting/train.py
+++ model_detection_2/project/model_fitting/train.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+from itertools import chain
+from operator import methodcaller
+import pprint
 import torch
 import os
 from model_fitting.losses import YoloLoss
@@ -8,6 +12,7 @@ from visualization.images_display import join_images
 from model_fitting.configuration import TrainingConfiguration
 from torchsummary import summary
 from torchvision.transforms.functional import to_pil_image
+from torchmetrics.detection.mean_ap import MeanAveragePrecision
 
 from visualization.output_transform import TargetTransformToBoxes
 
@@ -29,6 +34,7 @@ def fit_epoch(net, dataloader, lr_rate, train, epoch=1):
     total_offset_loss = 0.0
     total_class_loss = 0.0
     images = []
+    map_metrics = MeanAveragePrecision(box_format="xywh", class_metrics=True)
     for i, data in enumerate(tqdm(dataloader)):
         image, labels = data
         if train:
@@ -44,7 +50,8 @@ def fit_epoch(net, dataloader, lr_rate, train, epoch=1):
         losses += loss.item()
         total_offset_loss += offset_loss
         total_class_loss += class_loss
-
+        if not train:
+            new_func(net, box_transform, map_metrics, labels, outputs)
         if i>=len(dataloader)-5:
             outs = [out[0].cpu().detach().unsqueeze(0).numpy() for out in outputs]
             labs = [labels[0].cpu()[0].numpy()]
@@ -52,7 +59,42 @@ def fit_epoch(net, dataloader, lr_rate, train, epoch=1):
             images.append(pilImage)
         
     data_len = len(dataloader)
+    if not train:
+        pprint.pprint(map_metrics.compute())
+        
     return losses/data_len, total_objectness_loss/data_len, total_size_loss/data_len, total_offset_loss/data_len, total_class_loss/data_len, images
+
+def new_func(net, box_transform, map_metrics, labels, outputs):
+    box_labels = box_transform([labels[0].cpu()[0].numpy()])
+    box_outs = [out[0].cpu().detach().unsqueeze(0).numpy() for out in outputs]
+    boxes_pr = []
+    for l, out in enumerate(box_outs):
+        boxes_pr += box_transform(out, threshold = 0.20, depth = l)
+    labs = box_to_torch(box_labels, net.classes)
+    map_metrics.update([box_to_torch(boxes_pr, net.classes)], [labs])
+
+def box_to_torch(boxes: list, classes: list):
+    dd = dict(
+        boxes=[],
+        scores=[],
+        labels=[]
+    )
+    boxes = [box_form(b, classes)for b in boxes]
+    dict_items = map(methodcaller('items'), boxes)
+    for k, v in chain.from_iterable(dict_items):
+        dd[k].append(v)
+    dd = {k: torch.tensor(v) for k, v in dd.items()}
+    
+    return dd
+
+def box_form(box: dict, classes: list):
+    out = dict(
+        boxes=box["bbox"],
+        scores=box["confidence"],
+        labels=classes.index(box["category"])
+    )
+    
+    return out
 
 def fit(net, trainloader, validationloader, dataset_name, epochs=1000, lower_learning_period=10):
     model_dir_header = net.get_identifier()
@@ -88,12 +130,13 @@ def fit(net, trainloader, validationloader, dataset_name, epochs=1000, lower_lea
         else:
             train_config.iteration_age+=1
             print('Epoch {} metric: {}'.format(epoch, loss))
-        if train_config.iteration_age and (train_config.iteration_age % lower_learning_period) == 0:
-            train_config.learning_rate*=0.5
-            print("Learning rate lowered to {}".format(train_config.learning_rate))
         if train_config.iteration_age == 1 + lower_learning_period:
             net.grad_backbone(True)
             print("Model unfrozen")
+        if train_config.iteration_age and (train_config.iteration_age % lower_learning_period) == 0:
+            train_config.learning_rate*=0.5
+            print("Learning rate lowered to {}".format(train_config.learning_rate))
+        
         train_config.epoch = epoch+1
         train_config.save(checkpoint_conf_path)
         torch.save(net, checkpoint_name_path)
